376c5e203b0f9a5d9f2fc7af3b9a23f233ca9ab9
Shravan Shah <shravan91@users.noreply.github.com>
2015-05-25 17:16:39 +0530

    updating users

--- query/add_user.sql
+++ query/add_user.sql
@@ -33,6 +33,36 @@ select add_user(2901820,'udaykanth','008000',true);
 select add_user(2901753,'vamshikrishna','800000',true);
 select add_user(2897436,'venkatesh10','66CCCC',true);
 select add_user(2901818,'vudemraju','7B68EE',true);
-select add_user(2897087,'Yadhi06','ADFF2F',true);--UPDATE osm_user
+select add_user(2897087,'Yadhi06','ADFF2F',true);
+select add_user(2910514,'akhilsai','ADFF2F',true);
+select add_user(2907723,'anushapyata','ADFF2F',true);
+select add_user(2891150,'bhanu3','0171C5',true);
+select add_user(2907704,'kranthikumar','FF8A00',true);
+select add_user(2901764,'kushwanth','FF8A00',true);
+select add_user(2907853,'masthanvali','6ACCCC',true);
+select add_user(2901739,'pallavi1','6ACCCC',true);
+select add_user(2907752,'rajureddyvudem','6ACCCC',true);
+select add_user(2901508,'samuelmj','6ACCCC',true);
+select add_user(2913195,'sathishshetty','FA58F4',true);
+select add_user(2907738,'vamshiN','6ACCCC',true);
+select add_user(2915921,'venugopal009','3E8380',true);
+select add_user(2901490,'vikramsingh','6ACCCC',true);
+select add_user(2906661,'vraju','6ACCCC',true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--UPDATE osm_user
 --   SET color= '666600'
 -- WHERE iduser=0000;
